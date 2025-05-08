a0ffedbcf8319b2660ab4fced087fcb5d3aaa40f
ChandanaPrabhakar <154395526+ChandanaPrabhakar@users.noreply.github.com>
2025-05-08 17:02:50 +0530

    added in memory caching, setInterval to 15 seconds

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,17 @@
 import express from 'express';
 import {config} from 'dotenv';
+import { portfolioService } from './services/getPortfolioService';
 import routes from './routes/portfolioRoutes';
+import { holdings } from './models/portfolioModel';
 
 config();
 const app = express();
 
 app.use(express.json());
+
+portfolioService(holdings);
+setInterval(() => portfolioService(holdings), 15000);
+
 app.use('/api', routes);
 
 const PORT = process.env.PORT;
